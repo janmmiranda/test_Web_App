a7ca77ddbed902a1f713c01e7d05a3c00af1d032
Jan Matthew Miranda <janmatthewmiranda@Jans-MacBook-Pro.local>
2017-10-10 15:33:08 -0400

    Added functionality to Register Page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,24 +1,49 @@
 <template>
-  <div>
-    <h1>Register</h1>
-
-    <input 
-      type="email" 
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input 
-      type="password" 
-      name="password"
-      v-model="password"
-      placeholder="password" />
-    <br>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3 blue-grey--after>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan">
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+  
+        <div class="pl-4 pr-4 pt-2 pb-2 blue-grey flat dense">
+          <!-- <input 
+            type="email" 
+            name="email"
+            v-model="email"
+            placeholder="email" /> -->
+          <v-flex xs8 offset-xs2> 
+            <v-text-field
+              class="cyan--text text--lighten-2"
+              type="email" 
+              name="email"
+              label="Email"
+              v-model="email" />
+            <br>
+            <v-text-field
+              class="cyan--text text--lighten-2"
+              type="password" 
+              name="password"
+              v-model="password"
+              label="Password" />
+          </v-flex>
+          <!-- <input 
+            type="password" 
+            name="password"
+            v-model="password"
+            placeholder="password" /> -->
+          <br>
+          <div class="error" v-html="error" />
+          <br>
+          <v-btn
+            class="cyan"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -27,21 +52,30 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+.error {
+  color: red;
+}
+.dark {
+  color: ;
+}
 </style>
